a357950f2ec8a3474768a8e24a3bf4b5bfd2978d
kev200 <bleedeagle@gmail.com>
2016-02-20 12:04:23 +0200

    Fix for Mobile view

--- assets/libs/javascript.js
+++ assets/libs/javascript.js
@@ -1,17 +1,3 @@
-$(document).ready(function() {
-
-	$(window).scroll(function(event){
-
-		var y = $(this).scrollTop();
-
-		if (y >= 100) {
-
-			$('#mina').addClass('animated fadeInLeft opacity1');
-			$('.minustText').addClass('animated fadeInRight opacity1');
-		}
-	});
-});
-
 
 $(document).ready(function(){
 
@@ -24,15 +10,50 @@ $(document).ready(function(){
 		function(){
 		$(this).removeClass('animated fadeInRight');
 	});
-
-	// $('#mina').addClass('animated fadeInLeft').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-	// 	function(){
-	// 	$(this).removeClass('animated fadeInLeft');
-	// });
-
 });
 
-$(document).ready(function(){
-	$('.fromleft').addClass('animated fadeInLeft');
-	$('.fromright').addClass('animated fadeInLeft');
+// $(document).ready(function(){
+// 	$('.fromleft').addClass('animated fadeInLeft');
+// 	$('.fromright').addClass('animated fadeInLeft');
+// });
+
+
+$(document).ready(function() {
+
+if ( $(window).width() > 760) {
+
+	$(document).ready(function(){
+		$('.fromleft').addClass('animated fadeInLeft');
+		$('.fromright').addClass('animated fadeInLeft');
+	});
+
+  } 
+  else 
+  {
+    	$('.fromleft').addClass('opacity1');
+		$('.fromright').addClass('opacity1');
+  }
+});
+
+
+$(document).ready(function() {
+
+if ( $(window).width() > 760) {
+
+	$(window).scroll(function(event){
+    	var y = $(this).scrollTop();
+
+ 		if (y >= 100) {
+
+ 			$('#mina').addClass('animated fadeInLeft opacity1');
+ 			$('.minustText').addClass('animated fadeInRight opacity1');
+		}
+	});
+
+  } 
+  else 
+  {
+    	$('#mina').addClass('opacity1');
+		$('.minustText').addClass('opacity1');
+  }
 });
