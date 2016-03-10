4b1042de92361afe9546e6af5726c83dc2f8b009
kev200 <bleedeagle@gmail.com>
2016-03-10 21:12:30 +0200

    Updates

--- assets/libs/shrink.js
+++ assets/libs/shrink.js
@@ -1,18 +1,23 @@
 /* SHRINK MENU WHEN SCROLL */ 
 
 $(window).on("scroll touchmove", function () {
-	if($(document).scrollTop() > 0){
-		$('#page-wrapper').addClass('shrink');
-		$('.to-top').fadeIn(300);
-	}else{
-		$('#page-wrapper').removeClass('shrink');
-		$('.to-top').fadeOut(300);
+	if ( $(window).width() > 760) {
+		if($(document).scrollTop() > 0){
+			$('#page-wrapper').addClass('shrink');
+			$('.to-top').fadeIn(300);
+		}else{
+			$('#page-wrapper').removeClass('shrink');
+			$('.to-top').fadeOut(300);
+		}
 	}
 });
+
+
+
 
 
 /* TO TOP */
 
 $('.to-top').on('click', function(){
 	$('#page-wrapper').animate({ scrollTop: 0 }, 'fast');
-})
+});
